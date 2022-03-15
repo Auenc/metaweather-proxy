25f349213feeda8c4c67414e415951d893f10833
Lewis <ljoncb@gmail.com>
2022-03-15 17:37:14 +0000

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3001
+
+app.use(cors())
 
 app.use((req, res, next) => {
     const url = `https://www.metaweather.com${req.originalUrl}`
